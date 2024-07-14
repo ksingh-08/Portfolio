71b6a776eb10ba6992e3dfd912cb0270b79bcbbd
ksingh-08 <mailtoksingh08@gmail.com>
2024-07-14 13:59:38 +0530

    gap between projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -12,7 +12,7 @@ const RecentProjects = () => {
 
 
         </h1>
-        <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+        <div className='flex flex-wrap items-center justify-center p-4  gap-x-20 gap-y-20 mt-10 '>
             {projects.map(( {id ,title,des,img,iconLists,link})=>(
                 <div key={id} className='sm:h-[42rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                     <PinContainer
